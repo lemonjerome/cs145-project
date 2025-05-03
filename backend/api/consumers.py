826c260637d9dce951a1abcd44ccd5d29bb90862
lemonjerome <gabrielcatadmanramos@gmail.com>
2025-05-03 20:47:43 +0800

    feat: simulation webscokect connect moved to gpx page

--- backend/api/consumers.py
+++ backend/api/consumers.py
@@ -12,9 +12,4 @@ class SimulationConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         data = json.loads(text_data)
         coordinates = data.get('coordinates')
-        if coordinates:
-            print(f"Received coordinates: {coordinates}", flush=True)
-            # Echo the coordinates back to the client
-            await self.send(text_data=json.dumps({
-                'message': f"Coordinates received: {coordinates}"
-            }))
+        
