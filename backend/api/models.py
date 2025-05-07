d490d5755f59ffecc50f3cc8898f76e060f76669
Gerard Salao <ghsalao@up.edu.ph>
2025-05-08 03:00:02 +0800

    feat(backend): Stoplight and StoplightGroup models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class StoplightGroup(models.Model):
+  lat = models.FloatField()
+  lng = models.FloatField()
+
+  def __str__(self):
+    return f"Stoplight Group {self.id} at ({self.lat}, {self.lng})"
+  
+class Stoplight(models.Model):
+    group = models.ForeignKey(
+        StoplightGroup,
+        on_delete=models.CASCADE,
+        related_name="stoplights"
+    )
+    lat = models.FloatField()
+    lng = models.FloatField()
+    direction = models.CharField(
+        max_length=10,
+        choices=[
+            ('N', 'North'),
+            ('NE', 'Northeast'),
+            ('E', 'East'),
+            ('SE', 'Southeast'),
+            ('S', 'South'),
+            ('SW', 'Southwest'),
+            ('W', 'West'),
+            ('NW', 'Northwest')
+        ],
+        blank=True  # optional for now ; can neglect => purpose of blank=True
+    )
+
+    def __str__(self):
+        return f"Stoplight {self.id} in Group {self.group.id} ({self.direction})"
